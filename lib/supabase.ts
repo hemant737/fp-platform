6943184b9025dfc979afcb96264785d24af5931f
hemant737 <hemant@firstprinciples.org.in>
2026-05-19 00:31:13 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://rsdalmemuupccpjyrilh.supabase.co',
-  'PASTE_LEGACY_ANON_PUBLIC_KEY_HERE'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzZGFsbWVtdXVwY2NwanlyaWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMTgyMjgsImV4cCI6MjA5NDY5NDIyOH0.d94wtfUQgmDehk-3Da9E7zvoARZiWNSOl4ypgoDRueI'
 )
